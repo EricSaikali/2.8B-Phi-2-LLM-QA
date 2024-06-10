d7ae44e75c4f9a4dab95d7d49a696e771e5f3d73
ESmike0 <100021446+EricSaikali@users.noreply.github.com>
2024-06-10 22:38:47 +0200

    Create training.sh

---
+++ training/training.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --chdir /scratch/izar/saikali/mnlpproj4/training
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem-per-gpu 32G
+#SBATCH --time 24:00:00
+#SBATCH --gres gpu:1
+#SBATCH --account cs-552
+#SBATCH --qos cs-552
+#SBATCH --reservation cs-552
+
+# `SBATCH --something` is how you tell SLURM what resources you need
+# The --reservation line only works during the 2-week period
+# where 80 GPUs are available. Remove otherwise
+
+module load gcc
+module load python
+
+source ../../shaikespear_train/bin/activate
+
+python train.py --config_list_names training_31_05_Anthony
