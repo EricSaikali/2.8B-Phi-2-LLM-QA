f0e981140c9677ebe5280afe2e135c7b09613c5a
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2024-06-12 23:23:27 +0200

    Fixed utils

--- training/utils.py
+++ training/utils.py
@@ -5,14 +5,14 @@ def format_function(dataset_name="EPFL_SFT"):
     def format_EPFL_SFT(input):
         output_texts = []
         for i in range(len(input['prompt'])):
-            text = f"{input['prompt'][i]}\n\n### Explanation: {input['chosen'][i]}"
+            text = f"{input['prompt'][i]}\n\n ### Explanation: {input['chosen'][i]}"
             output_texts.append(text)
         return output_texts
 
     def format_MCQA(input):
         output_texts = []
         for i in range(len(input['question'])):
-            text = f"{input['question'][i]}\n\n### Explanation: {input['answer_text'][i]}\n\n### Answer: {input['answer'][i]}"
+            text = f"{input['question'][i]}\n\n ### Explanation: {input['answer_text'][i]}\n\n ### Answer: {input['answer'][i]}"
             output_texts.append(text)
         return output_texts
 
