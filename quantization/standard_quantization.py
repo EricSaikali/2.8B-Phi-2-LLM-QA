362e3a83d1d1dbde74eba965296a52f2b8f31b5f
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2024-06-13 23:13:18 +0200

    Quantization

--- quantization/standard_quantization.py
+++ quantization/standard_quantization.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
@@ -6,14 +8,21 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 # TODO check if this is what is demanded to compare.
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-    model_path = "../model/checkpoints/training_12_06_MERGED_MCQA/models/EPFL_DPO/final"
-    model_name = "training_12_06_MERGED_MCQA"
+    parser.add_argument("--model_path")
+    args = parser.parse_args()
+
+    model_path = args.model_path
+
+    model_parent_folder = '/'.join(model_path.split("/")[:-3])
+    model_name = model_path.split("/")[-2]
+
     tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     bnb_quantization_config = BitsAndBytesConfig(load_in_4bit=True,  bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4")
 
     model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=bnb_quantization_config)
 
-    model.save_pretrained(f"./model/checkpoints/{model_name}-quantized")
-    tokenizer.save_pretrained(f"./model/checkpoints/{model_name}-quantized")
+    model.save_pretrained(f"{model_parent_folder}/quantized/{model_name}")
+    tokenizer.save_pretrained(f"{model_parent_folder}/quantized/{model_name}")
